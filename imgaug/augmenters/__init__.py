1269e2134d8b0721c3758e7c461061a88282c706
aleju <kontakt@ajung.name>
2019-05-22 21:50:23 +0200

    Add AveragePool; Add augmenters/pool.py

--- imgaug/augmenters/__init__.py
+++ imgaug/augmenters/__init__.py
@@ -8,6 +8,7 @@ from imgaug.augmenters.convolutional import *
 from imgaug.augmenters.flip import *
 from imgaug.augmenters.geometric import *
 from imgaug.augmenters.meta import *
+from imgaug.augmenters.pool import *
 from imgaug.augmenters.segmentation import *
 from imgaug.augmenters.size import *
 from imgaug.augmenters.weather import *
